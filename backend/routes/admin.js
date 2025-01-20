ac353599ccd81a7f50c6f8d852ce9747ac997702
Martin Synák <martin.synak123@gmail.com>
2025-01-20 18:44:31 +0100

    ADD: api for admin logout

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import Admin from '../models/admin.model.js';
+import jwt from 'jsonwebtoken';
+import Blacklist from '../models/blackList.model.js';
+
 import { checkAuth, createJSONToken, isValidPassword } from '../utils/auth.js';
 import User from '../models/user.model.js';
 import Company from '../models/company.model.js';
@@ -24,7 +27,7 @@ router.post('/login', async (req, res) => {
         
         res.status(200).json({
         message: 'Login successful',
-        payload: { user: userWithoutPassword, token, exp },
+        payload: { admin: userWithoutPassword, token, exp },
         });
     }catch(error){
         res.status(500).json({ message: error.message });
@@ -44,6 +47,49 @@ router.get('/length', checkAuth, async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+router.get('/user/:userEmail', checkAuth, async (req, res) => {
+    const { userEmail } = req.params;
+    try {
+        const user = await User.find({ email: userEmail }).select('firstName email');
+        if(user.length > 0) return res.status(200).json({ message: 'User fetched successfully', payload: {user:{userName: user[0].firstName, email: user[0].email}} });
+
+        const company = await Company.find({ email: userEmail }).select('companyName email');
+        if(company.length > 0) return res.status(200).json({ message: 'Company fetched successfully', payload: {user:{userName: company[0].companyName, email: company[0].email}} });
+
+
+        res.status(404).json({ message: 'User not found' });
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/logout', checkAuth, async (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+  
+    if (!token) {
+      return res.status(400).json({ message: 'Token is required' });
+    }
+  
+    // Decode the token to get its expiration time
+    const decoded = jwt.decode(token);
+  
+    if (!decoded || !decoded.exp) {
+      return res.status(400).json({ message: 'Invalid token' });
+    }
+  
+    const expiryDate = new Date(decoded.exp * 1000); // Convert exp from seconds to milliseconds
+  
+    // Save token in the blacklist with expiration
+    await Blacklist.create({ token, createdAt: expiryDate });
+    await Blacklist.deleteMany({ createdAt: { $lte: new Date() } });
+
+  
+    res.status(200).json({ message: 'Logged out successfully' });
+});
+
 
 
 
