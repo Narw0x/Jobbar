356dd259e0134d5ed68e3f4f7531f1beb4449413
Martin Synák <martin.synak123@gmail.com>
2025-01-28 21:58:38 +0100

    ADD: smtp transport

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,16 +1,20 @@
 import nodeamiler from "nodemailer";
+import smtpTransport from "nodemailer-smtp-transport";
 import emailRegistration from "./emailRegistration.js";
 
 export default async function sendEmail(options){
-    const transporter = nodeamiler.createTransport({
+    const transporter = nodeamiler.createTransport(smtpTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
         secure: false,
+        tls: {
+            rejectUnauthorized: false,
+        },
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASSWORD,
         },
-    });
+    }));
     
     const message = {
         from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
